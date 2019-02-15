4793c7b40b0c923b75f346c09e41ac80aa71f22b
ILKO <Travelingflyboy@gmail.com>
2019-02-15 22:28:21 +0200

    TELADRESS

---
+++ src/main/java/ua/org/ostpc/ittools/controller/TelegramusController.java
@@ -0,0 +1,21 @@
+package ua.org.ostpc.ittools.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+
+@Controller
+public class TelegramusController {
+
+
+    @GetMapping(value = "telegrampage")
+    public ModelAndView displayTelegramus(ModelAndView mav, HttpSession session, HttpServletRequest request) {         //ModelAndView return??
+
+        mav.setViewName("TelegramPage");
+        return mav;
+    }
+}
